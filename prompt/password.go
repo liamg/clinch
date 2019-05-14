26e764403e8af2add70bf977e608fb16a747601c
Liam Galvin <liam@liam-galvin.co.uk>
2019-05-14 13:36:33 +0100

    fix password entry

--- prompt/password.go
+++ prompt/password.go
@@ -1,38 +1,21 @@
 package prompt
 
 import (
-	"bufio"
-	"os"
-	"strings"
+	"syscall"
+	"fmt"
 
 	"github.com/liamg/clinch/terminal"
-	"github.com/liamg/tml"
+	sshterm "golang.org/x/crypto/ssh/terminal"
 )
 
 // EnterPassword requests input from the user with the given message, hiding that input, and returns any user input that was gathered until a newline was entered
 func EnterPassword(msg string) string {
-
 	terminal.ClearLine()
 	terminal.PrintImportantf(msg)
-
-	parser := tml.NewParser(os.Stdout)
-	parser.IncludeTrailingResets = false
-	parser.Parse(strings.NewReader("<hidden>"))
-	terminal.HideCursor()
-	defer func() {
-		parser.Parse(strings.NewReader("</hidden>"))
-		terminal.ShowCursor()
-	}()
-
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil || len(input) <= 1 {
+	bytePassword, err := sshterm.ReadPassword(int(syscall.Stdin))
+	if err != nil {
 		return ""
 	}
-
-	s := input[:len(input)-1]
-	if s[len(s)-1] == '\r' {
-		s = input[:len(input)-1]
-	}
-	return s
+	fmt.Printf("\n")
+	return string(bytePassword)
 }
